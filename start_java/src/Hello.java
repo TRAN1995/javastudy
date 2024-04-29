372bc46e1a6054508f0417f25aeab3e9487c08c0
tran1995 <havantran19957@gmail.com>
2024-04-29 14:47:35 +0700

    First Project

---
+++ start_java/src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Tim");
+        System.out.println("Hello World");
+        boolean isAlien = false;
+        if(isAlien==false){
+            System.out.println("It is not an alien!");
+        }
+    }
+}
